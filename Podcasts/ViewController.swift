1d8856b7a38755a1b07838668c7c1a10ea2bdd36
ayeo <me@ayeo.pl>
2019-11-16 10:16:25 +0100

    Initial

---
+++ Podcasts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by Michał Grabowski on 16/11/2019.
+//  Copyright © 2019 Michał Grabowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
