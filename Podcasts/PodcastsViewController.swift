c5c03f004cce57519a7c7d7d5e786c047fdd9b7a
ayeo <me@ayeo.pl>
2019-11-16 10:53:40 +0100

    Dev

---
+++ Podcasts/PodcastsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PodcastsViewController.swift
+//  Podcasts
+//
+//  Created by Michał Grabowski on 16/11/2019.
+//  Copyright © 2019 Michał Grabowski. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :osx, '10.15.1'
+use_frameworks!
+
+target 'Podcasts' do
+  pod 'SWXMLHash', '~> 5.0.0'
+end
